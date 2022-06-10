b8629742e797e72e563d07aeb5672268c957914d
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-06-10 10:33:53 +0200

    Add Vec3 and utils

---
+++ raytracer/util/Vec3.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class Vec3 {
+public:
+    Vec3()
+        : e { 0, 0, 0 }
+    {
+    }
+    Vec3(double e0, double e1, double e2)
+        : e { e0, e1, e2 }
+    {
+    }
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    Vec3 operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    Vec3& operator+=(Vec3 const& v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    Vec3& operator*=(double const t)
+    {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    Vec3& operator/=(double const t)
+    {
+        return *this *= 1 / t;
+    }
+
+    double length() const
+    {
+        return sqrt(length_squared());
+    }
+
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+public:
+    double e[3];
+};
+
+// Type aliases for Vec3
+using point3 = Vec3; // 3D point
+using color = Vec3;  // RGB color
+
+// Vec3 Utility Functions
+
+inline std::ostream& operator<<(std::ostream& out, Vec3 const& v)
+{
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline Vec3 operator+(Vec3 const& u, Vec3 const& v)
+{
+    return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline Vec3 operator-(Vec3 const& u, Vec3 const& v)
+{
+    return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline Vec3 operator*(Vec3 const& u, Vec3 const& v)
+{
+    return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline Vec3 operator*(double t, Vec3 const& v)
+{
+    return Vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline Vec3 operator*(Vec3 const& v, double t)
+{
+    return t * v;
+}
+
+inline Vec3 operator/(Vec3 v, double t)
+{
+    return (1 / t) * v;
+}
+
+inline double dot(Vec3 const& u, Vec3 const& v)
+{
+    return u.e[0] * v.e[0]
+        + u.e[1] * v.e[1]
+        + u.e[2] * v.e[2];
+}
+
+inline Vec3 cross(Vec3 const& u, Vec3 const& v)
+{
+    return Vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline Vec3 unit_vector(Vec3 v)
+{
+    return v / v.length();
+}
