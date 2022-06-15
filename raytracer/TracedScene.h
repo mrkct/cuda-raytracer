2e803fd596866374256239420df92668c1b93760
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-06-15 15:44:39 +0200

    Change image dimensions from int to unsigned

--- raytracer/TracedScene.h
+++ raytracer/TracedScene.h
@@ -4,22 +4,23 @@
 #include <cstdint>
 #include <memory>
 
-
-class TracedScene
-{
-    public:
-    TracedScene(int width, int height, uint8_t *data)
-        : m_width(width), m_height(height), m_data(data)
-    {}
+class TracedScene {
+public:
+    TracedScene(unsigned int width, unsigned int height, uint32_t* data)
+        : m_width(width)
+        , m_height(height)
+        , m_data(data)
+    {
+    }
 
     int width() const { return m_width; }
     int height() const { return m_height; }
-    static int bytes_per_pixel() { return 3; }
-    uint8_t const* pixel_data() const { return m_data.get(); }
+    static int bytes_per_pixel() { return 4; }
+    uint32_t const* pixel_data() const { return m_data.get(); }
 
-    private:
-    int m_width, m_height;
-    std::unique_ptr<uint8_t[]> m_data;
+private:
+    unsigned int m_width, m_height;
+    std::unique_ptr<uint32_t[]> m_data;
 };
 
 #endif
