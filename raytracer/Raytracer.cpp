1e34e9487d7e105ab0fbda86fa49e128a6cd2a86
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-06-16 07:58:13 +0200

    Sphere hit test

--- raytracer/Raytracer.cpp
+++ raytracer/Raytracer.cpp
@@ -8,6 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+__device__ bool hit_sphere(Point3 const& center, double radius, Ray const& r)
+{
+    Vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 __global__ void calculateRay(uint32_t* framebuffer, int image_width, int image_height)
 {
     int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -18,11 +28,12 @@ __global__ void calculateRay(uint32_t* framebuffer, int image_width, int image_h
 
     uint32_t* pixel = &framebuffer[(image_height - row - 1) * image_width + col];
 
-    auto const aspect_ratio = image_width / image_height;
+    auto const aspect_ratio = (float)image_width / image_height;
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
 
+    // FIXME: The origin should go into constant memory
     auto origin = Point3(0, 0, 0);
     auto horizontal = Vec3(viewport_width, 0, 0);
     auto vertical = Vec3(0, viewport_height, 0);
@@ -32,9 +43,13 @@ __global__ void calculateRay(uint32_t* framebuffer, int image_width, int image_h
     auto v = double(row) / (image_height - 1);
     Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 
-    Vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    *pixel = ((1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0)).make_rgba();
+    if (hit_sphere(Point3(0.0, 0.0, -1.0), 0.5, r)) {
+        *pixel = Color(1.0, 0.0, 0.0).make_rgba();
+    } else {
+        Vec3 unit_direction = unit_vector(r.direction());
+        auto t = 0.5 * (unit_direction.y() + 1.0);
+        *pixel = ((1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0)).make_rgba();
+    }
 }
 
 TracedScene Raytracer::trace_scene(Scene const&)
