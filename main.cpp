f30268078fc1bc0626e7f6f593a8e5580aa774f8
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-06-20 17:20:40 +0200

    Multiple frame rendering test

--- main.cpp
+++ main.cpp
@@ -60,14 +60,33 @@ int main(int argc, char** argv)
               << "\tOutput: " << args.output_path << std::endl;
 
     auto raytracer = Raytracer(args.image_width, args.image_height);
+#define DEG2RAD(d) (d * M_PI / 1.80f)
+    auto render_start_time = std::chrono::high_resolution_clock::now();
+    auto frame_only_time = std::chrono::milliseconds();
+    for (int i = 0; i < 180; i += 30) {
+        auto frame_start_time = std::chrono::high_resolution_clock::now();
+        auto traced_scene = raytracer.trace_scene(
+            { 2.0 * cosf(DEG2RAD(i)), 0, -1.0 }, { 0, 0, -1 },
+            TestScene::init);
+        auto frame_finish_time = std::chrono::high_resolution_clock::now();
+        auto frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(frame_finish_time - frame_start_time);
 
-    auto start_time = std::chrono::high_resolution_clock::now();
-    auto traced_scene = raytracer.trace_scene(
-        { -2, 2, 1 }, { 0, 0, -1 },
-        TestScene::init);
-    auto finish_time = std::chrono::high_resolution_clock::now();
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(finish_time - start_time);
+        std::cout << "Frame " << i << "\tTime: " << frame_time.count() << "ms" << std::endl;
+        frame_only_time += frame_time;
 
-    std::cout << "Elapsed time: " << elapsed_time.count() << "ms" << std::endl;
-    return traced_scene.write_to_file(args.output_path);
+        auto frame_id = i < 10 ? "0" + std::to_string(i) : std::to_string(i);
+        auto output_path = std::string(args.output_path) + frame_id + ".png";
+        std::cout << "Writing frame to " << output_path << std::endl;
+        auto rc = traced_scene.write_to_file(output_path.c_str());
+        if (rc) {
+            std::cerr << "Failed to write frame " << i << " to disk due to error code " << rc << std::endl;
+        }
+    }
+
+    auto total_rendering_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - render_start_time);
+    std::cout << "Render finished" << std::endl
+              << "\tTotal Time:\t" << total_rendering_time.count() << std::endl
+              << "\tRender-only Time:\t" << frame_only_time.count() << std::endl;
+
+    return 0;
 }
