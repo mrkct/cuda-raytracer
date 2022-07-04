21eb2b436a13dbafedfb3f17e2da13bc17cacfcb
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-07-04 16:15:54 +0200

    adjust fov in planets.h

--- scenes/planets.h
+++ scenes/planets.h
@@ -6,7 +6,7 @@
 
 #define PLANETS_CAMERA_FROM make_vec3(-40, 35, -10)
 #define PLANETS_CAMERA_LOOKAT make_vec3(-1, 0, 30)
-#define PLANETS_CAMERA_FOV 10
+#define PLANETS_CAMERA_FOV 30
 
 struct Scene create_planets_scene(void);
 
