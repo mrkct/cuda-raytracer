9ce8041ca3170b780c89e8d4e67022c4bc850233
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-07-07 11:11:14 +0200

    inline some stuff for performance

--- util/rng.cu
+++ util/rng.cu
@@ -1,18 +1,6 @@
 #include <math.h>
 #include <util/rng.h>
 
-__device__ float rng_next(curandState_t* state)
-{
-    // curand_uniform returns between the range (0.0, 1] but we
-    // actually prefer the range [0.0, 1.0)
-    return 1.0 - curand_uniform(state);
-}
-
-__device__ float rng_next_in_range(curandState_t* state, float min, float max)
-{
-    return min + rng_next(state) * (max - min);
-}
-
 __device__ vec3 rng_next_in_unit_sphere(curandState_t* state)
 {
     // See: https://karthikkaranth.me/blog/generating-random-points-in-a-sphere/
