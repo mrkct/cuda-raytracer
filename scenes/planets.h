750b3dc53d8d3a6ca9dc09e312d11255f56397c2
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-07-08 13:23:54 +0200

    test

--- scenes/planets.h
+++ scenes/planets.h
@@ -5,7 +5,7 @@
 #include <scenes/scene.h>
 
 #define PLANETS_CAMERA_FROM make_vec3(-40, 35, -10)
-#define PLANETS_CAMERA_LOOKAT make_vec3(-1, 0, 30)
+#define PLANETS_CAMERA_LOOKAT make_vec3(-35, 30, -5)
 #define PLANETS_CAMERA_FOV 30
 
 /**
